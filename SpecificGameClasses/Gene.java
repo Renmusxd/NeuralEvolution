1e3477bcbef7c16eb59a1574103772450264bc6c
Renmusxd <sumnernh@gmail.com>
2013-12-09 23:00:51 -0500

    Gene operations

--- SpecificGameClasses/Gene.java
+++ SpecificGameClasses/Gene.java
@@ -12,7 +12,7 @@ public class Gene {
     // primer gene pair class
     char primer;
     String gene;
-    public Gene(char Primer, String gene){
-        
-    }
+    public Gene(char primer, String gene){this.primer = primer; this.gene = gene;}
+    public char getPrimer() {return primer;}
+    public String getGene() {return gene;}
 }
